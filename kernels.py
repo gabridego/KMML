9430b7671d420bd31b89f6f77e553e37cb3075dc
degolag <gabriele.degola@grenoble-inp.org>
2021-12-12 19:04:50 +0100

    optimize linear kernel

--- kernels.py
+++ kernels.py
@@ -15,15 +15,11 @@ class LinearKernel(Kernel):
         super().__init__(X, gamma)
 
     def similarity_matrix(self):
-        l = len(self.X)
-        K = np.empty([l, l])
-        for i in range(l):
-            for j in range(i, l):
-                K[i, j] = K[j, i] = np.dot(self.X[i], self.X[j])
+        K = self.X @ self.X.T
         return K
 
     def similarity(self, x):
-        return np.array([np.dot(x_i, x) for x_i in self.X])
+        return x @ self.X.T
 
 
 class GaussianKernel(Kernel):
